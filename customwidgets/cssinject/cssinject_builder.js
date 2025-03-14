54531c40518311410889c12070ca659d19000e91
icamac <84896034+icamac@users.noreply.github.com>
2025-03-15 00:35:00 +1100

    Update cssinject_builder.js

--- customwidgets/cssinject/cssinject_builder.js
+++ customwidgets/cssinject/cssinject_builder.js
@@ -3,11 +3,6 @@
 	template.innerHTML = `
 		<div class="sapEpmUiFormSection expandable expanded">
 			<div class="sapEpmUiFormSectionHeader">
-				<div class="sapEpmUiFormSectionExpandableIconContainer">
-					<button class="sapEpmUiFormSectionExpandableIcon">
-						<span data-sap-ui="__icon1017" data-sap-ui-render="" role="presentation" aria-hidden="true" aria-label="down" data-sap-ui-icon-content="" class="sapUiIcon sapUiIconMirrorInRTL" style="font-family: fpa-icons; transform: rotate(360deg); max-width: 360px;"></span>
-					</button>
-				</div>
 				<label class="sapEpmUiFormSectionTitle expandableTitle">CSSInject Properties</label>
 			</div>
 			<div class="sapEpmUiFormSectionItems">

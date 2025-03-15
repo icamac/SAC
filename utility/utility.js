cb1270ee931e48bbc734864c6a8f79f0f8afd5d9
icamac <84896034+icamac@users.noreply.github.com>
2025-03-15 23:19:01 +1100

    Update utility.js
    added getEnvironment method

--- utility/utility.js
+++ utility/utility.js
@@ -8,7 +8,8 @@ class Utility extends HTMLElement {
 		// Expose methods for SAC scripting
 		this._methods = {
 			generateUUID: this.generateUUID.bind(this),
-			formatDate: this.formatDate.bind(this)
+			formatDate: this.formatDate.bind(this),
+			getEnvironment: this.getEnvironment.bind(this)
 		};
 	}
 
@@ -24,6 +25,17 @@ class Utility extends HTMLElement {
 
 		const options = { year: "numeric", month: "2-digit", day: "2-digit" };
 		return new Intl.DateTimeFormat("en-GB", options).format(d);
+	}
+
+	// Determine the environment based on the host
+	getEnvironment() {
+		const host = window.location.host;
+
+		if (host.includes("csiro-q.ap10.hcs.cloud.sap")) return "Dev";
+		if (host.includes("csiro-q-1")) return "QA";
+		if (host.includes("csiro-1")) return "Prod";
+
+		return "Unknown";
 	}
 
 	// Expose methods for SAC
